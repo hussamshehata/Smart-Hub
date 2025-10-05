6596e86461a248039e7d1df72bf88d060cefad7f
AmiraAshraf2 <stud21std@gmail.com>
2025-10-06 00:44:00 +0300

    Add shipping from and Contact Information from

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 // src/App.jsx
 import React from "react";
 import ProductButtons from "@/components/ProductButtons"; // or "./components/ProductButtons" if you don't use @ alias
-
+import Shipping from "@/components/shippingForm";
+import ContactInformation from "@/components/ContactInformation";
 function App() {
     return (
         <div className="p-8">
             <h1 className="text-2xl font-heading mb-4">Product</h1>
             {/* other product details here */}
             <ProductButtons />
+            <ContactInformation />
+            <Shipping />
+
         </div>
     );
 }
