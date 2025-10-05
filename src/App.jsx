b7ef9e8981e070e39e05c30af12fc052bcada38e
AmiraAshraf2 <164785782+AmiraAshraf2@users.noreply.github.com>
2025-10-06 01:08:41 +0300

    Revert "Add shipping from and Contact Information from"

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 // src/App.jsx
 import React from "react";
 import ProductButtons from "@/components/ProductButtons"; // or "./components/ProductButtons" if you don't use @ alias
-import Shipping from "@/components/shippingForm";
-import ContactInformation from "@/components/ContactInformation";
 import ProductCard from "./Components/ProductCard";
 function App() {
     return (
         <div className="p-8">
             <h1 className="text-2xl font-heading mb-4">Product</h1>
             {/* other product details here */}
-            <ProductButtons />
-            <ContactInformation />
-            <Shipping />
-
             <ProductCard/>
         </div>
     );
