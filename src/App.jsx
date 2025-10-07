6805f8bce1f05e1d9575b8b5b9c1d0daf5382b3e
hussam shehata <hossam.sh126@gmail.com>
2025-10-08 01:11:39 +0300

    updating toggle buttons it is still need updates but it is more than fine right now and rearranges the files and pages and fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,26 @@
 // src/App.jsx
 import React from "react";
-import ProductButtons from "@/components/ProductButtons"; // or "./components/ProductButtons" if you don't use @ alias
-import CheckoutDetails from "./pages/CheckoutDetails";
-import Home from "./pages/Home";
-
-
-
-
-function App() {
-    return (
-        <>
-            <CheckoutDetails />
-        </>
-import Shipping from "@/components/shippingForm";
-import ContactInformation from "@/components/ContactInformation";
 import ProductCard from "./Components/ProductCard.jsx";
 import Header from "./components/Header"
 import GridSection from "@/Components/GridSection.jsx";
+import ThemeToggle from "@/Components/ThemeToggle.jsx";
 
 function App() {
     return (
         <GridSection>
 
-<div>
+            <div>
+                <Header/>
 
-            <Header/>
-            {/* other product details here */}
-            <ProductButtons />
-            <ContactInformation />
-            <Shipping />
+<ThemeToggle/>
+                {/* other product details here */}
 
-            <ProductCard/>
-</div>
+
+                <ProductCard/>
+
+            </div>
         </GridSection>
+
 
     );
 }
