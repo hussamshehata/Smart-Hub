63a05c1c422b62ad3fa572781d1cad0d0510ff39
Asmaa A. Mostafa <asmaamostafa0511@gmail.com>
2025-10-08 17:55:13 +0300

    added 4 components
    created hero component .
    created brands component .
    created promotions component .
    created newsletter components .

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,29 @@ import Navbar from "./Components/Navbar&Footer/Navbar.jsx"
 import GridSection from "@/Components/GridSection.jsx";
 import ThemeToggle from "@/Components/All-Buttons/ThemeToggle.jsx";
 import Footer from "@/Components/Navbar&Footer/Footer.jsx"
+
+import HeroBanner from "./Components/HeroBanner.jsx";
+import Brands from "./Components/Brands.jsx";
+import PromoBanner from "./Components/promoBanner.jsx";
+import Newsletter from "./Components/Newsletter.jsx";
 function App() {
     return (
         <GridSection>
 
             <div>
                 <Navbar/>
+                <HeroBanner/>
+                <Brands/>
 
 <ThemeToggle/>
                 {/* other product details here */}
 
 
                 <ProductCard/>
+                <PromoBanner/>
+                <Newsletter/>
+
+                
 <Footer/>
             </div>
         </GridSection>
