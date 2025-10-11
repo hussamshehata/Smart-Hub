c60be2a0fa414fa2eab50af2f1a145972eca5861
Asmaa A. Mostafa <asmaamostafa0511@gmail.com>
2025-10-11 20:39:28 +0300

    edited image
    edited image in newslwtter component

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -1,6 +1,6 @@
 function Newsletter() {
   return (
-    <div className="newsletter bg-[url('src/assets/newsletter.jpg')]" >
+    <div className=" bg-[url('src/assets/newsletter.jpg')]  bg-cover  bg-center  bg-no-repeat" >
 
         <div className="newsletter">
             <div className="flex flex-col items-center justify-center py-16">
@@ -9,7 +9,7 @@ function Newsletter() {
 
                 <form className="text-2xl mb-8">
                     <input type="email" placeholder="Enter your email" className="p-2 w-80 border border-gray-400 bg-transparent rounded" />
-                    <button className="bg-transparent border border-gray-500 text-gray-600 px-4 py-2 rounded cursor-pointer">Signup</button>
+                    <button className="bg-transparent border border-gray-400 text-gray-600 px-4 ml-4 py-2 rounded cursor-pointer">Signup</button>
                 </form>
 
                  <hr />
