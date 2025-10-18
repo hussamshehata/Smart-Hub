5592a4f7be51076d25d5a98a93d07ad228630787
Asmaa A. Mostafa <asmaamostafa0511@gmail.com>
2025-10-18 13:41:29 +0300

    called 4components in home page
    called
    HeroBanner ,
    PromoBannner ,
    Services ,
    NewsLetter
    in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,14 @@ import Navbar from "../Components/Navbar&Footer/Navbar.jsx";
 import ProductCard from "../components/ProductCard";
 import ProductButtons from "../Components/All-Buttons/productbuttons.jsx";
 
+import HeroBanner from "@/Components/HeroBanner";
+import PromoBanner from "@/Components/PromoBanner";
+import Services from "@/Components/Services";
+import Newsletter from "@/Components/Newsletter";
+
 export default function Home() {
     return (
+<<<<<<< Updated upstream
         <>
             <Navbar />
             <div className="p-8">
@@ -16,5 +22,19 @@ export default function Home() {
                 <ProductCard />
             </div>
         </>
+=======
+        <motion.div
+            className="p-6"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.4 }}
+        >
+            <HeroBanner />
+            <PromoBanner />
+            <Services />
+            <Newsletter /> 
+        </motion.div>
+>>>>>>> Stashed changes
     );
 }
