32ced1be75d2c44b646831cac57dc5dc77887dd6
Asmaa A. Mostafa <asmaamostafa0511@gmail.com>
2025-10-21 07:52:55 +0300

    enhanced some styles
    enhanced some styles in home components

--- src/Components/Brands.jsx
+++ src/Components/Brands.jsx
@@ -1,31 +1,43 @@
 function Brands() {
     return (
-        <div className="w-full mt-16">
-            <ul className="mx-8 list-none flex justify-between py-8 px-8 text-xl">
+        <div className="mx-40 py-10">
+            <ul className="list-none flex justify-between text-xl">
 
-                <li className="flex flex-row text-gray-500 text-xl">
-                <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 1" className="w-8 h-auto mr-2" />
-                Apple</li>
+                <li className="flex flex-row text-neutral-700 text-xl">
+                    <a href="#Apple" className="flex items-center gap-2 hover:underline">
+                    <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 1" className="w-8 h-auto " />
+                    Apple</a>
+                </li>
 
-                <li className="flex flex-row text-gray-500 text-xl">
-                <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 2" className="w-8 h-auto mr-2" />
-                Samsung</li>
+                <li className="flex flex-row text-neutral-700 text-xl">
+                    <a href="#Samsung" className="flex items-center gap-2 hover:underline">
+                    <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 2" className="w-8 h-auto " />
+                    Samsung</a>
+                </li>
 
-                <li className="flex flex-row text-gray-500 text-xl">
-                <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 3" className="w-8 h-auto mr-2" />
-                Oppo</li>
+                <li className="flex flex-row text-neutral-700 text-xl">
+                    <a href="#Oppo" className="flex items-center gap-2 hover:underline">
+                    <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 3" className="w-8 h-auto " />
+                    Oppo</a>
+                </li>
 
-                <li className="flex flex-row text-gray-500 text-xl">
-                <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 4" className="w-8 h-auto mr-2" />
-                Huawei</li>
+                <li className="flex flex-row text-neutral-700 text-xl">
+                    <a href="#Huawei" className="flex items-center gap-2 hover:underline">
+                    <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 4" className="w-8 h-auto " />
+                    Huawei</a>
+                </li>
 
-                <li className="flex flex-row text-gray-500 text-xl">
-                <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 5" className="w-8 h-auto mr-2" />
-                Sony</li>
+                <li className="flex flex-row text-neutral-700 text-xl">
+                    <a href="#Sony" className="flex items-center gap-2 hover:underline">
+                    <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 5" className="w-8 h-auto " />
+                    Sony</a>
+                </li>
 
-                <li className="flex flex-row text-gray-500 text-xl">
-                <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 6" className="w-8 h-auto mr-2" />
-                Poco</li>
+                <li className="flex flex-row text-neutral-700 text-xl">
+                    <a href="#Poco" className="flex items-center gap-2 hover:underline">
+                    <img src="https://res.cloudinary.com/dbaqz7nim/image/upload/v1760885950/image_s1abl3.jpg" alt="Brand 6" className="w-8 h-auto " />
+                    Poco</a>
+                </li>
 
             </ul>
         </div>
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,26 +1,26 @@
 function Services () {
     return ( 
-        <div className="flex justify-center py-16 ">
-            <div className="flex gap-8">
-                <div className="bg-gray-200 p-4 w-56 h-48 flex flex-col justify-center">
-                    <img src="service.jpg" className="w-8 h-8"/>
-                    <h4 className="my-4 font-bold">Free Shipping</h4>
-                    <p className="description">Orders over $50</p>
+        <div className="flex justify-center mx-40 py-10">
+            <div className="flex gap-8 w-full">
+                <div className=" bg-gray-200 p-4 w-1/4 h-48 flex flex-col justify-center gap-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="  text-gray-700 " width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-truck-icon lucide-truck"><path d="M14 18V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v11a1 1 0 0 0 1 1h2"/><path d="M15 18H9"/><path d="M19 18h2a1 1 0 0 0 1-1v-3.65a1 1 0 0 0-.22-.624l-3.48-4.35A1 1 0 0 0 17.52 8H14"/><circle cx="17" cy="18" r="2"/><circle cx="7" cy="18" r="2"/></svg>
+                    <h4 className="text-2xl text-neutral-700">Free Shipping</h4>
+                    <p className="text-sm text-neutral-500">Orders over $50</p>
                 </div>
-                <div className="bg-gray-200 p-4 w-56 h-48 flex flex-col justify-center">
-                    <img src="service.jpg" className="w-8 h-8"/>
-                    <h4 className="my-4 font-bold">Money Back Guarantee</h4>
-                    <p className="description">30 day guarantee</p>
+                <div className="bg-gray-200 p-4 w-1/4 h-48 flex flex-col justify-center gap-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="  text-gray-700 " width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-banknote-icon lucide-banknote"><rect width="20" height="12" x="2" y="6" rx="2"/><circle cx="12" cy="12" r="2"/><path d="M6 12h.01M18 12h.01"/></svg>
+                    <h4 className="text-2xl text-neutral-700">Money Back</h4>
+                    <p className="text-sm text-neutral-500">30 day guarantee</p>
                 </div>
-                <div className="bg-gray-200 p-4 w-56 h-48 flex flex-col justify-center">
-                    <img src="service.jpg" className="w-8 h-8"/>
-                    <h4 className="my-4 font-bold">Secure Payment</h4>
-                    <p className="description">Secured by Stripe</p>
+                <div className="bg-gray-200 p-4 w-1/4 h-48 flex flex-col justify-center gap-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="   text-gray-700 " width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lock-keyhole-icon lucide-lock-keyhole"><circle cx="12" cy="16" r="1"/><rect x="3" y="10" width="18" height="12" rx="2"/><path d="M7 10V7a5 5 0 0 1 10 0v3"/></svg>
+                    <h4 className="text-2xl text-neutral-700">Secure Payment</h4>
+                    <p className="text-sm text-neutral-500">Secured by Stripe</p>
                 </div>
-                <div className="bg-gray-200 p-4 w-56 h-48 flex flex-col justify-center">
-                    <img src="service.jpg" className="w-8 h-8"/>
-                    <h4 className="my-4 font-bold">24/7 Support</h4>
-                    <p className="description">Phone and Email support</p>
+                <div className="bg-gray-200 p-4 w-1/4 h-48 flex flex-col justify-center gap-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="  text-gray-700 " width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone-icon lucide-phone"><path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384"/></svg>
+                    <h4 className="text-2xl text-neutral-700">24/7 Support</h4>
+                    <p className="text-sm text-neutral-500">Phone and Email support</p>
                 </div>
             </div>
         </div>
