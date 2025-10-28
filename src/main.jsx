1d41a35d68abc5cfc0494c0522ebd8f20237b9cb
hussam shehata <hossam.sh126@gmail.com>
2025-10-28 23:29:32 +0300

    Activate user login successful

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
-import "./index.css"; // if you’re using Tailwind
+import store from "@/redux/Store.js";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
