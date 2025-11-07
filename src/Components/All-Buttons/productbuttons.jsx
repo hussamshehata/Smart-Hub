4922aa49a085538fcdd8f44c6bc22230dd535773
esmail mostafa esmail <esmailalazaly2002@gmail.com>
2025-11-07 18:36:23 +0200

    handle cart with redux

--- src/Components/All-Buttons/productbuttons.jsx
+++ src/Components/All-Buttons/productbuttons.jsx
@@ -1,10 +1,14 @@
 import { Button } from "@/Components/ui/button.jsx"
+import { addToCart } from "@/redux/Cartslice";
 import { ShoppingCart, Heart,} from "lucide-react"
+import { useDispatch } from "react-redux";
 
-export default function ProductButtons() {
+export default function ProductButtons({ product }) {
+
+     const dispatch = useDispatch();
     return (
         <div className="flex items-center gap-2 mt-2">
-            <Button variant="default" size="sm">
+            <Button   variant="default" size="sm" onClick={() => { dispatch(addToCart(product));}}>
                 <ShoppingCart size={22} className="me-2" />
                 Add to cart
             </Button>
--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./UserSlice.js";
+import cartReducer from "./Cartslice.js";
 
 const store = configureStore({
     reducer: {
         user: userReducer,
+        cart: cartReducer
     },
 });
 
 export default store;
+
