ee1db22d0129c1022c669f86c54d21bb1d3e1e04
hussam shehata <hossam.sh126@gmail.com>
2025-11-11 16:56:26 +0200

    handling errors
    This reverts commit ab11f979

---
+++ server/utils/errorHandler.js
@@ -0,0 +1,14 @@
+// server/utils/errorHandler.js
+
+const errorHandler = (err, req, res) => {
+    console.error("❌ Error:", err.message);
+
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+    res.status(statusCode).json({
+        success: false,
+        message: err.message,
+        stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+};
+export default errorHandler
