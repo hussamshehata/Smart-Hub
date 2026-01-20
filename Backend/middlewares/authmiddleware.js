7f2791b689b8c14929187e2964e23acdf3512f30
hussam shehata <hossam.sh126@gmail.com>
2026-01-20 23:50:41 +0200

    Revert "Complete Auth System"
    This reverts commit 58499e86a1ef95a97ca8eab649022e9a7c04e6d8.

--- Backend/middlewares/authmiddleware.js
+++ Backend/middlewares/authmiddleware.js
@@ -1,6 +1,6 @@
 // middleware/auth.js
 import jwt from "jsonwebtoken";
-import User from "../models/user.js";
+import User from "../models/User.js";
 import rateLimit from "express-rate-limit";
 
 // ==================== AUTHENTICATION MIDDLEWARE ====================
